7ef027494ef1870f720ec29bf51076c3c372ea97
Shanwer <Shanwer@qq.com>
2023-04-01 20:12:28 +0800

    完成数据库存储用户账户

--- Server/src/A33Project/Main.java
+++ Server/src/A33Project/Main.java
@@ -2,11 +2,15 @@ package A33Project;
 
 import io.javalin.Javalin;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Objects;
 
 public class Main {
-    public static void main(String[] args) {
+    //TODO:参数中加入验证码之类的来防止恶意注册
+    public static void main(String[] args) throws NoSuchAlgorithmException {
         double startTime = System.nanoTime();
+        MessageDigest md5 = MessageDigest.getInstance("MD5");
         System.out.println("初始化数据库中...");
         try{
             if(args[0].equals("-DBPath") && args[1] != null) {
@@ -14,14 +18,22 @@ public class Main {
                 Javalin PostHandler = Javalin.create(/*config*/)
                     .post("/register", ctx -> {
                         if((Objects.equals(ctx.formParam("type"), "register"))) {
-                            SQLiteJDBCObj.register(ctx.formParam("username"), ctx.formParam("password"));
+                            byte[] inputByteArray = Objects.requireNonNull(ctx.formParam("password")).getBytes();
+                            md5.update(inputByteArray);
+                            byte[] resultByteArray = md5.digest();//对密码进行MD5加密，增强安全性
+                            //System.out.println("密码加密成功");
+                            SQLiteJDBCObj.register(ctx.formParam("username"),byteArrayToHex(resultByteArray));
                         }
-                        ctx.status(201);
+                        ctx.status(201);//创建成功
                     })
                     .post("/login", ctx -> {
                         if(Objects.equals(ctx.formParam("type"),"login")){
-                            SQLiteJDBCObj.login(ctx.formParam("username"), ctx.formParam("password"));
+                            byte[] inputByteArray = Objects.requireNonNull(ctx.formParam("password")).getBytes();
+                            md5.update(inputByteArray);
+                            byte[] resultByteArray = md5.digest();
+                            SQLiteJDBCObj.login(ctx.formParam("username"), byteArrayToHex(resultByteArray));
                         }
+                        ctx.status(201);
                     })
                     .start(8080);
                 double jsonEndTime = System.nanoTime();
@@ -33,5 +45,15 @@ public class Main {
             System.out.println("请输入参数！用法：java -jar A33Server.jar -DBPath 数据库文件的路径");
         }
 
+    }
+    public static String byteArrayToHex(byte[] byteArray) {
+        char[] hexDigits = {'0','1','2','3','4','5','6','7','8','9', 'A','B','C','D','E','F' };
+        char[] resultCharArray =new char[byteArray.length * 2];
+        int index = 0;
+        for (byte b : byteArray) {
+            resultCharArray[index++] = hexDigits[b>>> 4 & 0xf];
+            resultCharArray[index++] = hexDigits[b& 0xf];
+        }
+        return new String(resultCharArray);//返回一个长度为32的字符串
     }
 }
---
+++ Server/src/A33Project/SQLiteJDBC.java
@@ -0,0 +1,86 @@
+package A33Project;
+
+import java.sql.*;
+public class SQLiteJDBC {
+    //负责注册与登录，访问数据库
+    //TODO:判断账号和密码长度
+    private Connection c = null;
+    private ResultSet rs;
+    private static int Last_ID;
+
+    public SQLiteJDBC(String DBPath) {
+        try {
+            Statement stmt;
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection(DBPath);
+            stmt = c.createStatement();
+            String sql = "SELECT MAX(ID) FROM USER";//SELECT LAST_INSERT_ROWID() FROM USER会有异常
+            rs = stmt.executeQuery(sql);
+            Last_ID = rs.getInt(1);
+            //System.out.println(Last_ID);
+            rs.close();
+            stmt.close();
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("数据库初始化成功！");
+    }
+
+    public void register(String username,String password) {
+        if(username!=null && password!=null) {
+            try {
+                PreparedStatement preStmt;
+                //TODO:日后需要注意防范SQL注入
+                String registerSql = "INSERT INTO USER (ID,USERNAME,PASSWORD) " +
+                        "values(?,?,?)";
+                String checkDuplicatedNameSql = "SELECT * FROM USER WHERE USERNAME = ?";
+                preStmt = c.prepareStatement(checkDuplicatedNameSql);
+                preStmt.setString(1,username);
+                rs = preStmt.executeQuery();//只会用ResultSet的笨办法查重了
+                if(rs.next()){
+                    preStmt.close();
+                    rs.close();
+                    //TODO:重复用户名，做点啥
+                }else{
+                    preStmt = c.prepareStatement(registerSql);
+                    //System.out.println("SQL语句预编译成功");
+                    preStmt.setInt(1,++Last_ID);
+                    preStmt.setString(2,username);
+                    preStmt.setString(3,password);
+                    preStmt.execute();
+                    preStmt.close();
+                    rs.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+                System.out.println("数据库执行异常");
+            }
+        }
+    }
+    public void login(String username,String password){
+        if(username!=null && password!=null){
+            try {
+                Statement stmt;
+                //日后需要防范SQL注入
+                String sql = "SELECT USERNAME,PASSWORD FROM USER";
+                String usernameInDB = null,passwordInDB = null;
+                stmt = c.prepareStatement(sql);
+                rs = stmt.executeQuery(sql);
+                while(rs.next()){
+                    usernameInDB = rs.getString("username");
+                    passwordInDB = rs.getString("password");
+                }//TODO:这里换成数据库内查询操作效率会更高
+                if(username.equals(usernameInDB)&&password.equals(passwordInDB)){
+                    //TODO:登陆成功后的操作
+                }
+                rs.close();
+                stmt.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+                System.out.println("数据库异常");
+            }
+        }
+
+    }
+}
